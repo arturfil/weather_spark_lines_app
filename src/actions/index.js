9376016b5affd3e81de9a971b38c0e9e07af3fbf
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2017-07-10 23:35:45 -0400

    changed http to https in actions/index.js file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'bd7ef3719665e88d73f6f2bb68ba4ddc';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
