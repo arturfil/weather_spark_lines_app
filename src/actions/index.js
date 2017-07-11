ca7b854ee82de2e6b2b4943b64f5943f52c8cb97
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2017-07-10 23:28:48 -0400

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'bd7ef3719665e88d73f6f2bb68ba4ddc';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
