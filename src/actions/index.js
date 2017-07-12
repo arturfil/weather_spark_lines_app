8ce377dcaf64776f1a9c76827be9c2985eb49817
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2017-07-11 22:34:11 -0400

    changing the url of the api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_KEY = 'bd7ef3719665e88d73f6f2bb68ba4ddc';
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const API_KEY = '6b1207df7e54546111a98d724d55ffe3';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
